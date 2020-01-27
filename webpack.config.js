a5e33880c612e82c79b5deb8fdc8f23713c38ff0
greenn-lab <greenn.lab@gmail.com>
2020-01-27 23:40:22 +0900

    ignores eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
+/* eslint-disable */
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
 
 module.exports = {
     entry: './src/index.ts',
